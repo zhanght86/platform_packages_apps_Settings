CarbonROM/android_packages_apps_Settings

Settings: DisplaySettings: hide doze entry under display if target has custom doze settings
Huge thanks to @Myself5 for helping me with my crappy java knowledge.

Change-Id: I1442bc68cfc6f2fbf92137c56681bebe83aa1209

src/com/android/settings/DisplaySettings.java
@@ -50,6 +50,7 @@ import android.util.Log;
 import com.android.internal.app.NightDisplayController;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.util.cr.CrUtils;
 import com.android.internal.view.RotationPolicy;
 import com.android.settings.accessibility.ToggleFontSizePreferenceFragment;
 import com.android.settings.dashboard.SummaryLoader;
@@ -138,6 +139,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
         final Activity activity = getActivity();
         final ContentResolver resolver = activity.getContentResolver();
+        final boolean isCrDozeInstalled = CrUtils.isPackageInstalled(activity, "org.carbonrom.settings.doze");
 
         addPreferencesFromResource(R.xml.display_settings);
 
@@ -170,7 +172,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             removePreference(KEY_LIFT_TO_WAKE);
         }
 
-        if (isDozeAvailable(activity)) {
+        if (isDozeAvailable(activity) && !isCrDozeInstalled) {
             mDozePreference = (SwitchPreference) findPreference(KEY_DOZE);
             mDozePreference.setOnPreferenceChangeListener(this);
         } else {

zhanght86/platform_packages_apps_Settings

src/com/android/settings/DisplaySettings.java
@@ -47,6 +47,7 @@ import android.util.Log;
 import com.android.internal.app.NightDisplayController;
 import com.android.internal.logging.MetricsLogger;
 import com.android.internal.logging.MetricsProto.MetricsEvent;
+import com.android.internal.util.jdcteam.jdcUtils;
 import com.android.internal.view.RotationPolicy;
 import com.android.settings.accessibility.ToggleFontSizePreferenceFragment;
 import com.android.settings.dashboard.SummaryLoader;
@@ -111,6 +112,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
         super.onCreate(savedInstanceState);
         final Activity activity = getActivity();
         final ContentResolver resolver = activity.getContentResolver();
+        final boolean isDeviceDozeInstalled = jdcUtils.isPackageInstalled(activity, "com.cyanogenmod.settings.doze");
 
         addPreferencesFromResource(R.xml.display_settings);
 
@@ -143,7 +145,7 @@ public class DisplaySettings extends SettingsPreferenceFragment implements
             removePreference(KEY_LIFT_TO_WAKE);
         }
 
-        if (isDozeAvailable(activity)) {
+        if (isDozeAvailable(activity) && !isDeviceDozeInstalled) {
             mDozePreference = (SwitchPreference) findPreference(KEY_DOZE);
             mDozePreference.setOnPreferenceChangeListener(this);
         } else {
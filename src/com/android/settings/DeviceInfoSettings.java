04b1348b084463303109a4c68329f5a4f7ee3a39
B--B <dark.schneider.ds@gmail.com>
2017-08-05 00:44:14 +0200

    (3/3) Settings: add device maintainers

--- src/com/android/settings/DeviceInfoSettings.java
+++ src/com/android/settings/DeviceInfoSettings.java
@@ -75,6 +75,7 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
     private static final String PROPERTY_MBN_VERSION = "persist.mbn.version";
     private static final String KEY_QGP_VERSION = "qgp_version";
     private static final String PROPERTY_QGP_VERSION = "persist.qgp.version";
+    private static final String KEY_DEVICE_MAINTAINERS = "device_maintainer";
 
     static final int TAPS_TO_BE_A_DEVELOPER = 7;
 
@@ -123,6 +124,8 @@ public class DeviceInfoSettings extends SettingsPreferenceFragment implements In
         setStringSummary(KEY_BUILD_NUMBER, Build.DISPLAY);
         findPreference(KEY_BUILD_NUMBER).setEnabled(true);
         setValueSummary(KEY_QGP_VERSION, PROPERTY_QGP_VERSION);
+        // Device Maintainers
+        setValueSummary(KEY_DEVICE_MAINTAINERS, "ro.device.maintainer");
         // Remove QGP Version if property is not present
         removePreferenceIfPropertyMissing(getPreferenceScreen(), KEY_QGP_VERSION,
                 PROPERTY_QGP_VERSION);
